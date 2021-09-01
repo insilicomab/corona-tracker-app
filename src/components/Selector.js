0ea8bd2a7ffc33a2efd4dcd6827588eee70276cb
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-09-01 15:23:27 +0900

    add script

---
+++ src/components/Selector.js
@@ -0,0 +1,13 @@
+import countriesJson from "../countries.json";
+
+const Selector =()=> {
+    return(
+        <div>
+            <select>
+                <option></option>
+            </select>
+        </div>
+    );
+};
+
+export default Selector;
---
+++ src/pages/TopPage.js
@@ -0,0 +1,13 @@
+import Selector from "../components/Selector";
+import Title from "../components/Title";
+
+const TopPage =()=> {
+    return(
+        <div>
+            <Title />
+            <Selector />
+        </div>
+    );
+};
+
+export default TopPage;
