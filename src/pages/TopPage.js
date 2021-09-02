03b6a06d922ac59d2878806596ac4fd6d5d9bc02
insilicomab <54536339+insilicomab@users.noreply.github.com>
2021-09-02 10:03:03 +0900

    add script

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -1,11 +1,13 @@
 import Selector from "../components/Selector";
 import Title from "../components/Title";
+import Results from "../components/Results"
 
-const TopPage =()=> {
+const TopPage =(props)=> {
     return(
         <div>
             <Title />
-            <Selector />
+            <Selector countriesJson={props.countriesJson} setCountry={props.setCountry} getCountryData={props.getCountryData} />
+            <Results />
         </div>
     );
 };
